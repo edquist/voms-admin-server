3a8447633731820af0ea76724628f4360aed3b3d
Carl Edquist <edquist@cs.wisc.edu>
2017-03-27 15:36:35 -0500

    port OrgDBSessionFactory.java changes from 01064e71f (SOFTWARE-2652)

--- src/main/java/org/glite/security/voms/admin/integration/orgdb/database/OrgDBSessionFactory.java
+++ src/main/java/org/glite/security/voms/admin/integration/orgdb/database/OrgDBSessionFactory.java
@@ -30,8 +30,8 @@ import org.glite.security.voms.admin.integration.orgdb.model.Participation;
 import org.glite.security.voms.admin.integration.orgdb.model.VOMSOrgDBPerson;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.context.ThreadLocalSessionContext;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,9 +42,9 @@ public class OrgDBSessionFactory {
 	private static volatile SessionFactory orgDbSessionFactory;
 	
 	
-	public static AnnotationConfiguration buildConfiguration(Properties orgDbHibernateProperties){
+	public static Configuration buildConfiguration(Properties orgDbHibernateProperties){
 		
-		AnnotationConfiguration cfg = new AnnotationConfiguration()
+		Configuration cfg = new Configuration()
 			.addAnnotatedClass(Country.class)
 			.addAnnotatedClass(Experiment.class)
 			.addAnnotatedClass(Institute.class)
@@ -72,7 +72,7 @@ public class OrgDBSessionFactory {
 		
 		try{
 			
-			AnnotationConfiguration cfg = buildConfiguration(orgbHibernateProperties);
+			Configuration cfg = buildConfiguration(orgbHibernateProperties);
 			
 			orgDbSessionFactory = cfg.configure().buildSessionFactory();
 		
